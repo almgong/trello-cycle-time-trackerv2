b8b00f858ebc823fa2e09b69d6d5aded43a08a31
Allen Gong <alm.gong@gmail.com>
2019-10-04 14:41:08 -0700

    Use Rately to serially execute trello requests
    https://github.com/almgong/rately

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -10,6 +10,13 @@ var IN_PROGRESS_CARD_LABEL_PREFIX = 'In progress for';
 var COMPLETED_CARD_LABEL_PREFIX = 'Completed in';
 var Promise = window.TrelloPowerUp.Promise;
 
+// no more than 10 operations per second
+var serialExecutor = new window.Rately.default.SerialRatelyExecutor({
+  maxOperationsPerInterval: 5,  // 100 is the max, see https://developers.trello.com/docs/rate-limits
+  rateLimitIntervalMs: 500, // 1/2 seconds
+  bufferMs: 300
+});
+
 // prompt user to allow this power up to make requests on their behalf
 window.Trello.authorize({
   type: 'popup',
@@ -38,8 +45,9 @@ function buildBadge(timeElapsedHrs, desiredCycleTime, labelPrefix) {
   };
 }
 
-function computeTimeElapsedInHrs(startAsDate) {
-  return Math.floor((new Date() - startAsDate) / 3600000);
+function computeTimeElapsedInHrs(startAsDate, referenceDate) {
+  referenceDate = referenceDate || new Date();
+  return Math.floor((referenceDate - startAsDate) / 3600000);
 }
 
 function presentTimeElapsed(timeElapsedHrs) {
@@ -77,14 +85,18 @@ function parseMostRecentMoveToOneOfListsFromActions(actions, listSuffix, inverse
 
   var mostRecentDate = now;
   for (var i = 0; i < actions.length; i++) {
-    if (actions[i].data
-        && actions[i].data.listBefore
-        && actions[i].data.listAfter
-        && actions[i].data.listBefore.name
-        && actions[i].data.listAfter.name
-        && inverseCheck ? actions[i].data.listBefore.name.endsWith(listSuffix) : !actions[i].data.listBefore.name.endsWith(listSuffix)
-        && inverseCheck ? !actions[i].data.listAfter.name.endsWith(listSuffix) : actions[i].data.listAfter.name.endsWith(listSuffix)
+    if (!(actions[i].data
+      && actions[i].data.listBefore
+      && actions[i].data.listAfter
+      && actions[i].data.listBefore.name
+      && actions[i].data.listAfter.name)
     ) {
+        continue;
+    }
+
+    var movementFromListCondition = inverseCheck ? actions[i].data.listBefore.name.endsWith(listSuffix) : !actions[i].data.listBefore.name.endsWith(listSuffix);
+    var movementToListCondition = inverseCheck ? !actions[i].data.listAfter.name.endsWith(listSuffix) : actions[i].data.listAfter.name.endsWith(listSuffix);
+    if (movementFromListCondition && movementToListCondition) {
       mostRecentDate = new Date(actions[i].date);
       break;
     }
@@ -101,8 +113,69 @@ function isCompletionList(settings, listId) {
   return listId === settings.desiredCompletionList;
 }
 
-function isBacklogList(settings, listId, listName) {
-  return !isCycleTimeList(settings, listName) && !isCompletionList(settings, listId);
+function isBacklogList(settings, listName) {
+  return listName.endsWith(settings.desiredRtListSuffix);
+}
+
+function renderCardBadge(t) {
+  return t.get('board', 'shared', SETTINGS_STORAGE_KEY).then(function(storedSettings) {
+    if (!storedSettings) {
+      return PROMPT_FOR_SETTINGS_BADGE;
+    }
+
+    var settings = JSON.parse(storedSettings);
+
+    return t.list('id', 'name').then(function(list) {
+      return t.card('id').get('id').then(function(cardId) {
+        // if in backlog list, render a NULL badge
+        // else if in cycle time list render active badge
+        // else render completed badge
+        // if (listId === settings.backlogList) {
+        if (isBacklogList(settings, list.name)) {
+          console.log('backlog list, nulling...');
+
+          return t.remove('card', 'shared', INITIAL_CARD_START_TIME_STORAGE_KEY).then(function() {
+            return t.remove('card', 'shared', CARD_COMPLETED_TIME_IN_HRS_STORAGE_KEY).then(function() {
+              return NULL_BADGE;
+            });
+          });
+      } else if (isCycleTimeList(settings, list.name)) { // render badge with time
+          return t.remove('card', 'shared', CARD_COMPLETED_TIME_IN_HRS_STORAGE_KEY).then(function() {
+            return t.get('card', 'shared', INITIAL_CARD_START_TIME_STORAGE_KEY, null).then(function(storedStartTime) {
+              if (storedStartTime) {
+                return computeTimeElapsedInHrs(new Date(storedStartTime));
+              }
+
+              return window.Trello.get('/card/' + cardId + '/actions?filter=updateCard').then(function(actions) {
+                var startTime = parseMostRecentMoveToOneOfListsFromActions(actions, settings.desiredCtListSuffix);
+                return t.set('card', 'shared', INITIAL_CARD_START_TIME_STORAGE_KEY, startTime).then(function() {
+                  return computeTimeElapsedInHrs(startTime);
+                });
+              });
+            }).then(function(hrsElapsed) {
+              return buildBadge(hrsElapsed, +settings.desiredCycleTime, IN_PROGRESS_CARD_LABEL_PREFIX);
+            });
+          });
+        } else { // render a completed badge
+          return t.get('card', 'shared', CARD_COMPLETED_TIME_IN_HRS_STORAGE_KEY, null).then(function(completedTimeHrs) {
+            if (completedTimeHrs !== null) {
+              return buildBadge(+completedTimeHrs, settings.desiredCycleTime, COMPLETED_CARD_LABEL_PREFIX);
+            }
+
+            return window.Trello.get('/card/' + cardId + '/actions?filter=updateCard').then(function(actions) {
+              var startTime = parseMostRecentMoveToOneOfListsFromActions(actions, settings.desiredCtListSuffix);
+              var completedTime = parseMostRecentMoveToOneOfListsFromActions(actions, settings.desiredCtListSuffix, true);
+              var hrsElapsed = computeTimeElapsedInHrs(startTime, completedTime);
+
+              return t.set('card', 'shared', CARD_COMPLETED_TIME_IN_HRS_STORAGE_KEY, hrsElapsed).then(function() {
+                return buildBadge(hrsElapsed, +settings.desiredCycleTime, COMPLETED_CARD_LABEL_PREFIX);
+              });
+            });
+          });
+        }
+      });
+    });
+  });
 }
 
 window.TrelloPowerUp.initialize({
@@ -117,62 +190,10 @@ window.TrelloPowerUp.initialize({
     return [
       {
         dynamic: function () {
-          return t.get('board', 'shared', SETTINGS_STORAGE_KEY).then(function(storedSettings) {
-            if (!storedSettings) {
-              return PROMPT_FOR_SETTINGS_BADGE;
-            }
-
-            var settings = JSON.parse(storedSettings);
-
-            return t.list('id', 'name').then(function(list) {
-              return t.card('id').get('id').then(function(cardId) {
-                // if in backlog list, render a NULL badge
-                // else if in cycle time list render active badge
-                // else render completed badge
-                // if (listId === settings.backlogList) {
-                if (isBacklogList(settings, list.id, list.name)) {
-                  console.log('backlog list, nulling...');
-
-                  return t.remove('card', 'shared', INITIAL_CARD_START_TIME_STORAGE_KEY).then(function() {
-                    return t.remove('card', 'shared', CARD_COMPLETED_TIME_IN_HRS_STORAGE_KEY).then(function() {
-                      return NULL_BADGE;
-                    });
-                  });
-                // } else if (settings.selectedCycleTimeLists.indexOf(listId) !== -1) { // in cycle time list, render badge with time
-              } else if (isCycleTimeList(settings, list.name)) { // render badge with time
-                  return t.remove('card', 'shared', CARD_COMPLETED_TIME_IN_HRS_STORAGE_KEY).then(function() {
-                    return t.get('card', 'shared', INITIAL_CARD_START_TIME_STORAGE_KEY, null).then(function(storedStartTime) {
-                      if (storedStartTime) {
-                        return computeTimeElapsedInHrs(new Date(storedStartTime));
-                      }
-
-                      return window.Trello.get('/card/' + cardId + '/actions?filter=updateCard').then(function(actions) {
-                        var startTime = parseMostRecentMoveToOneOfListsFromActions(actions, settings.desiredCtListSuffix);
-                        return t.set('card', 'shared', INITIAL_CARD_START_TIME_STORAGE_KEY, startTime).then(function() {
-                          return computeTimeElapsedInHrs(startTime);
-                        });
-                      });
-                    }).then(function(hrsElapsed) {
-                      return buildBadge(hrsElapsed, +settings.desiredCycleTime, IN_PROGRESS_CARD_LABEL_PREFIX);
-                    });
-                  });
-                } else { // render a completed badge
-                  return t.get('card', 'shared', CARD_COMPLETED_TIME_IN_HRS_STORAGE_KEY, null).then(function(completedTimeHrs) {
-                    if (completedTimeHrs !== null) {
-                      return buildBadge(+completedTimeHrs, settings.desiredCycleTime, COMPLETED_CARD_LABEL_PREFIX);
-                    }
-
-                    return window.Trello.get('/card/' + cardId + '/actions?filter=updateCard').then(function(actions) {
-                      var completedTime = parseMostRecentMoveToOneOfListsFromActions(actions, settings.selectedCycleTimeLists, true);
-                      var hrsElapsed = computeTimeElapsedInHrs(completedTime);
-
-                      return t.set('card', 'shared', CARD_COMPLETED_TIME_IN_HRS_STORAGE_KEY, hrsElapsed).then(function() {
-                        return buildBadge(hrsElapsed, +settings.desiredCycleTime, COMPLETED_CARD_LABEL_PREFIX);
-                      });
-                    });
-                  });
-                }
-              });
+          return new Promise(function (res, _rej) {
+            serialExecutor.add({
+              workFn: function() { return renderCardBadge(t); },
+              cbFn: res
             });
           });
         }
